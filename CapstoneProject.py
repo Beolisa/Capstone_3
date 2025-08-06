8ab22a911af888854646d2748c1780838babec20
Beolisa <120142658+Beolisa@users.noreply.github.com>
2025-08-06 08:38:29 +0000

    add capstone project

---
+++ CapstoneProject.py
@@ -0,0 +1,60 @@
+<style>
+    h1 {
+        color: red;
+    }
+    h1 {
+        font-size: 7em;
+    }
+    h2 {
+        /* Change the number to change the size of the header text. */
+        font-size: 3em;
+    }
+    h3 {
+        font-size: 1.5em;
+    }
+    li {
+        font-size:1.5em;
+    }
+</style>
+<h1>Wanted</h1>
+
+<!-- Find an image from the Image Gallery above, or use your own image link. -->
+<img src="https://www.codecombat.com/file/db/thang.type/54eb540b49fa2d5c905ddf1a/portrait.png" id="most-wanted">
+
+<!-- Who is wanted? -->
+<h2>Bomb Bill</h2>
+
+<!-- What are they wanted for? -->
+<h3>For Reckless, Inhuman Crimes:</h3>
+<ul>
+    <li>Exploding</li>
+    <li>Rolling</li>
+    <li>Priming</li>
+    <li>Smoldering</li>
+</ul>
+
+<!-- What is the reward? -->
+<h2>Reward: Some Gold</h2>
+
+<!-- Who are their partners in crime? -->
+<h3>Known Accomplices</h3>
+<div>
+    <div>
+        <img src="https://www.codecombat.com/file/db/thang.type/5432f9d18364d30000d1f943/portrait.png">
+        <h3>Chest o' Gems</h3>
+    </div>
+    
+    <div>
+        <img src="https://www.codecombat.com/file/db/thang.type/53024c7b27471514685d5397/portrait.png">
+        <h3>The Hand</h3>
+    </div>
+    
+    <!-- Add a third accomplice here. Choose whether they are class "captured". -->
+    <div>
+        <img src="http://direct.codecombat.com/file/db/thang.type/52e95a5022efc8e709001743/portrait.png">
+        <h3>cow</h3>
+    </div>
+</div>
+
+<!-- How do want them captured? -->
+<h2>Capture Alive Only</h2>
